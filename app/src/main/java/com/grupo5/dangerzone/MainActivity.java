eac8fad9945955a748dfa9f1489143e7ad0434fa
Fabian Sierra <frsierrag@gmail.com>
2021-07-09 22:50:24 -0500

    1. Se crea clase Lugar 2. Se crea clase GeoPunto 3. Se crea clase TipoLugar 4. Se modifica MainActivity, se adicionan las librerías y se guarda el campo R.id en el activity_main.xml

--- app/src/main/java/com/grupo5/dangerzone/MainActivity.java
+++ app/src/main/java/com/grupo5/dangerzone/MainActivity.java
@@ -1,14 +1,34 @@
 package com.grupo5.dangerzone;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Tag in msg", "Mensaje de prueba");
     }
+ */
+
+    TextView mensaje;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Lugar lugar = new Lugar("UIS","Clle 9#27",7.1377,-73.121,
+                TipoLugar.EDUCACION, 6344000, "https://www.uis.edu.co/",
+                "Una de las mejores universidades de Colombia", 5);
+        mensaje = findViewById(R.id.txt_mensaje);
+        mensaje.setText(lugar.toString());
+        Log.d("Tag in msg", "Mensaje de prueba por el logcat: " + lugar.toString());
+    }
+
+
+
 }
